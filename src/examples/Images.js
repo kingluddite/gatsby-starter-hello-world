cd45ebcada7b0d9b213ae937980ef28b8b04791d
Kingluddite <howley.phil@gmail.com>
2020-08-03 14:27:31 -0400

    Add Images

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -19,6 +19,14 @@ const getImages = graphql`
         }
       }
     }
+    example: file(relativePath: { eq: "large-image.jpg" }) {
+      childImageSharp {
+        # default 800
+        fluid(maxWidth: 100) {
+          ...GatsbyImageSharpFluid_withWebp_tracedSVG
+        }
+      }
+    }
   }
 `;
 
@@ -30,17 +38,22 @@ const Images = () => {
       <article className="single-image">
         <h3>basic image</h3>
         <img src={img} alt="text" width="100%" />
+        <h2>Content</h2>
       </article>
       <article className="single-image">
         <h3>fixed image/blure</h3>
         <Image fixed={data.fixed.childImageSharp.fixed} />
+        <h2>Content</h2>
       </article>
       <article className="single-image">
         <h3>fluid image/svg</h3>
         <Image fluid={data.fluid.childImageSharp.fluid} />
+        <h2>Content</h2>
         <div className="small">
           <Image fluid={data.fluid.childImageSharp.fluid} />
+          <h2>Content</h2>
         </div>
+        <Image fluid={data.example.childImageSharp.fluid} />
       </article>
     </section>
   );
