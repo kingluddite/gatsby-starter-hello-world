240922a37ba31109550f7f5be21d433ca638aa3b
Kingluddite <howley.phil@gmail.com>
2020-08-08 23:01:15 -0400

    Add templates for gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const home = () => {
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>
+        Ipsum aperiam expedita veritatis voluptatibus reiciendis. Reprehenderit
+        eaque assumenda beatae quos totam earum Voluptatem dolor exercitationem
+        ducimus voluptas doloremque nesciunt Eligendi consequatur repudiandae
+        ipsum hic?
+      </p>
+    </Layout>
+  );
+};
+
+export default home;
---
+++ src/templates/product-template.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const productTemplate = (props) => {
+  console.log(props);
+  return <h1>Hello from product template</h1>;
+};
+
+export default productTemplate;
